cf7399e389bfc11d5f716406678ae8c7109980bb
Magda <magwalasek@gmail.com>
2016-11-13 21:14:44 +0100

    Triangle area task

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var a = prompt('Podaj długość boku:'),
+h = prompt('Podaj wysokość:'),
+triangleArea=(a*h)/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
